30fde69d95ad04f30160cfe305e1781ec9b8903a
KhoaNH <Khoa@ominext.com>
2023-04-16 00:18:00 +0700

    update code test layout

---
+++ my-app/src/views/header/header.js
@@ -0,0 +1,11 @@
+
+import logo from '../../img/logoNCC 1.svg'
+function Header() {
+  return (
+    <div className="header">
+   <img src={logo} alt="" />
+    </div>
+  );
+}
+
+export default Header;
---
+++ my-app/src/views/nav/nav.js
@@ -0,0 +1,16 @@
+
+function Nav() {
+  return (
+    <div className="nav">
+        <div className="nav-wrap">
+          <div className="menu-item active">Home</div>
+          <div className="menu-item">Services</div>
+          <div className="menu-item">News</div>
+          <div className="menu-item">Blog</div>
+          <div className="menu-item">Contact</div>
+        </div>
+    </div>
+  );
+}
+
+export default Nav;
